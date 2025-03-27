b4e4e5521b6ad4d082261bd73f1ab1c4712260ee
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-03-27 20:25:24 +0000

    Replaced getters and setters for inlines.

--- src/process/PCB.h
+++ src/process/PCB.h
@@ -4,35 +4,35 @@ class PCB
 {
 private:
 	static int next_pid; // Sequential id
-	int pid;			
-	int priority;		// Generated with uniform distribution/weighted random selection
-	int exec_time; // Countdown for time already executed
-	int deadline;		// For deadline based algorithms
-	int burst_time;		// CPU burst times -> if short: Exponential, else if variable: Normal
-	int arrival_time;	// Poisson/Exponential generated value + previous arrival time (example)
-	std::string name;	// For statistics
+	int pid;
+	int priority;	  // Generated with uniform distribution/weighted random selection
+	int exec_time;	  // Countdown for time already executed
+	int deadline;	  // For deadline based algorithms
+	int burst_time;	  // CPU burst times -> if short: Exponential, else if variable: Normal
+	int arrival_time; // Poisson/Exponential generated value + previous arrival time (example)
+	std::string name; // For statistics
+
 	// todo: Add more atributes as needed like 'periodic'...
 public:
-	PCB() : pid(next_pid++) {}
+	PCB() : pid(next_pid++), priority(0), exec_time(0), deadline(0), burst_time(0), arrival_time(0), name("") {}
 
 	// * getters ---------------------------------------------------------
-	int get_pid() const {return pid;}
-	int get_priority() const {return priority;}
-	int get_exec_time() const {return exec_time;}
-	int get_deadline() const {return deadline;}
-	int get_burst_time() const {return burst_time;}
-	int get_arrival_time() const {return arrival_time;}
-	std::string get_name() const {return name;}
+	inline int get_pid() const { return pid; }
+	inline int get_priority() const { return priority; }
+	inline int get_exec_time() const { return exec_time; }
+	inline int get_deadline() const { return deadline; }
+	inline int get_burst_time() const { return burst_time; }
+	inline int get_arrival_time() const { return arrival_time; }
+	inline std::string get_name() const { return name; }
 
 	// * setters ---------------------------------------------------------
 	// 'n' represents any new value for an atribute
-	void set_priority(int n) {priority = n;}
-	void set_exec_time(int n) {exec_time = n;}
-	void set_deadline(int n) {deadline = n;}
-	void set_burst_time(int n) {burst_time = n;}
-	void set_arrival_time(int n) {arrival_time = n;}
-	void set_name(std::string n) {name = n;}
-
+	inline void set_priority(int n) { priority = n; }
+	inline void set_exec_time(int n) { exec_time = n; }
+	inline void set_deadline(int n) { deadline = n; }
+	inline void set_burst_time(int n) { burst_time = n; }
+	inline void set_arrival_time(int n) { arrival_time = n; }
+	inline void set_name(std::string &n) { name = n; } // (little cool optimization)
 };
 
 int PCB::next_pid = 1;
