d5b410d63e653912969b248963a1689e9126e56a
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-04-02 12:01:22 +0100

    Simulation init

--- src/cpu_scheduler/scheduler.hpp
+++ src/cpu_scheduler/scheduler.hpp
@@ -4,6 +4,10 @@
 // Abstract class for scheduling algorithms. Works as an interface.
 class Scheduler
 {
+private:
+	static inline int current_time = 0;
+
+public:
 	// Here, functions are made pure virtual functions,
 	// which forces the implementation of the following functions
 	// in each scheduling algorithm (derived classes).
@@ -11,4 +15,5 @@ class Scheduler
 	virtual void add_pcb() = 0;
 	virtual void remove_pcb() = 0;
 	virtual PCB get_next_pcb() = 0;
+	inline void reset_time() { current_time = 0; }
 };
