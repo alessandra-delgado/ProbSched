27a08eb262b61864806243be4d118b0655095dcb
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-04-03 18:48:18 +0100

    Fcfs now runs (with some bugs to fix)

--- src/cpu_scheduler/algorithms/fcfs/fcfs.cpp
+++ src/cpu_scheduler/algorithms/fcfs/fcfs.cpp
@@ -20,47 +20,65 @@ void FCFS::remove_pcb()
         ready.pop();
     }
 }
-const PCB& FCFS::get_next_pcb()
+const PCB &FCFS::get_next_pcb()
 {
     if (ready.empty())
         throw std::runtime_error("No PCB in ready queue.");
-    const PCB &next_pcb = ready.top();
-    return next_pcb;
+    PCB *next_pcb = const_cast<PCB *>(&ready.top());
+    remove_pcb();
+    next_pcb->set_state(ProcessState::Running);
+    return *next_pcb;
 }
 
 void FCFS::schedule()
 {
     ProcessGenerator pg(0.85, 10.0, 3.0, 10);
     RandomGenerator rng;
-    while(true){
-        // todo: fix process state not changing to running or ready
-        // todo: fix processes not generating 
-        // todo: output to show what other processes are in the queue
-        // todo: fix process being ran when not on arrival time >:(
+    while (true)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        // ? todo: fix process running until 0
+        // todo: might have to fix really long burst times and process generation overall
+        // todo: organize/refactor this 
+        // Steps:
+        // 1 - Verify that there is a process running.
+        //      -> True: decrement execution time (no need to check other processes because there is no preemption!)
+        //              if it reaches some value below 0, then mark process as terminated (it is not in ready queue anyway)
+        //      -> False: generate a new process if calculated probability allows to
+        // If there is no process running and the queue is not empty, then get the next process.
+        // There is not preemption in fcfs.
+
         Scheduler::increment_current_time();
-        if (running_process)
+        if (running_process) // If there's a process running (pointer not null)
         {
             running_process->dec_exec_time();
-            if (running_process->get_exec_time() < 0){
+            if (running_process->get_exec_time() < 0)
+            {
                 running_process->set_state(ProcessState::Terminated);
                 running_process = nullptr;
             }
         }
-        if (is_ready_empty())
+
+        int e = static_cast<int>(rng.exponential(0.6));
+        if (e > 2) // Generate a random number to verify if a new process is created
         {
-            int e = static_cast<int>(rng.exponential(0.6));
-            if (e < 2) // Generate a random number to verify if a new process is created
-            {
-                std::cout << "Current time: " << Scheduler::get_current_time() << std::endl;
-                continue;
-            }
             PCB pcb = pg.generatePCB(Scheduler::get_current_time());
             add_pcb(pcb);
         }
-        if (!running_process && !is_ready_empty()){
-            running_process = const_cast<PCB*>(&get_next_pcb());
+
+        if (!running_process && !is_ready_empty())
+        {
+            running_process = const_cast<PCB *>(&get_next_pcb());
+            if(running_process->get_arrival_time() > Scheduler::get_current_time()){
+                add_pcb(*running_process); // Add the process again 
+                running_process = nullptr;
+                std::cout << "Current time: " << Scheduler::get_current_time() << std::endl;
+                continue;
+            }
         }
 
+        queue_processes();
+        // temporary statistics :)
         std::cout << "Current time: " << Scheduler::get_current_time()
                   << " | Process Running: " << (running_process ? running_process->get_name() : "NONE")
                   << " | Process State: " << (running_process ? to_string(running_process->get_state()) : "N/A")
@@ -68,8 +86,17 @@ void FCFS::schedule()
                   << " | Arrival time: " << (running_process ? running_process->get_arrival_time() : -1)
                   << " | Execution time: " << (running_process ? running_process->get_exec_time() : -1)
                   << std::endl;
-        std::this_thread::sleep_for (std::chrono::seconds(1));
-        if(Scheduler::get_current_time() > 60) return;
     }
 }
 
+void FCFS::queue_processes(){
+    std::cout << "=================================================================================================" << std::endl;
+    std::cout << "processos na ready queue:" << std::endl;
+    std::priority_queue<PCB, std::vector<PCB>, ArrivalTimeComparator> pq = ready;
+    while (!pq.empty()) {
+        std::cout << pq.top().get_name() << " ";
+        pq.pop();
+    }
+    std::cout << std::endl;
+    std::cout << "=====================================================" << std::endl;
+}
