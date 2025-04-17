cd4133261e0feed88098b70171d3637ee2ddb030
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-04-17 17:34:12 +0100

    Changed priority comparator. Piority algorithms seem to be running OK.

--- src/cpu_scheduler/algorithms/ps/priority_comparator.hpp
+++ src/cpu_scheduler/algorithms/ps/priority_comparator.hpp
@@ -3,6 +3,9 @@
 
 struct PriorityComparator{
     bool operator()(const PCB &p1, const PCB &p2){
-        return (p1.get_priority() < p2.get_priority()) && (p1.get_arrival_time() <= p2.get_arrival_time()); // processses from higher priority to lower
+        if(p1.get_priority() == p2.get_priority())
+            return p1.get_arrival_time() > p2.get_arrival_time();
+        // todo: test this comparator 
+        return (p1.get_priority() > p2.get_priority()); // processses from higher priority to lower
     }
 };
