02e0669a64b0c92c8d0b67134df00157c31f0574
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-04-17 18:24:27 +0100

    lol get formatted

--- src/cpu_scheduler/algorithms/rr/rr.hpp
+++ src/cpu_scheduler/algorithms/rr/rr.hpp
@@ -5,22 +5,22 @@
 #include <queue>
 #include <vector>
 
-class RoundRobin : public Scheduler {
-    private:
-        std::queue<PCB> ready; // queue with processes
-        int time_quantum;  // time quantum
+class RoundRobin : public Scheduler
+{
+private:
+    std::queue<PCB> ready; // queue with processes
+    int time_quantum;      // time quantum
 
-    public:
-        RoundRobin(int quantum) : time_quantum(quantum){}
-            
-            bool is_ready_empty() override;
-            void add_pcb(PCB pcb) override;
-            void remove_pcb() override;
-            const PCB get_next_pcb() override;
-            void schedule() override;
-            std::vector<PCB> ready_queue_to_vector() override;
+public:
+    RoundRobin(int quantum) : time_quantum(quantum) {}
 
-            void set_time_quantum(int quantum) { time_quantum = quantum; }
-            int get_time_quantum() const { return time_quantum; }
-        
+    bool is_ready_empty() override;
+    void add_pcb(PCB pcb) override;
+    void remove_pcb() override;
+    const PCB get_next_pcb() override;
+    void schedule() override;
+    std::vector<PCB> ready_queue_to_vector() override;
+
+    void set_time_quantum(int quantum) { time_quantum = quantum; }
+    int get_time_quantum() const { return time_quantum; }
 };
